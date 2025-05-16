f24103fd8b87346f3fea5a7e33d4fd31a0239c87
Ver0018 <rogerpantilyowger@gmail.com>
2025-05-17 02:37:49 +0800

    added auto import primevue components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts">
-import InputText from 'primevue/inputtext'
-import FloatLabel from 'primevue/floatlabel'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
     <h1 class="text-3xl font-bold underline">Home</h1>
 
     <FloatLabel>
+      <InputText id="over_label" />
       <InputText id="over_label" />
       <label for="over_label">Over Label</label>
     </FloatLabel>
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
