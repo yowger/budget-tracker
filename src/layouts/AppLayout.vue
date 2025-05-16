6b83af3ddedd8908c5314dfe0e3937cfe4043279
Ver0018 <rogerpantilyowger@gmail.com>
2025-05-17 03:19:09 +0800

    added base layout

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>topbar</div>
+    <div>sidebar</div>
+    <div>
+      <router-view></router-view>
+      <div>footer</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
+  <div class="bg-red-100">
     <h1 class="text-3xl font-bold underline">Home</h1>
 
     <FloatLabel>
       <InputText id="over_label" />
-      <InputText id="over_label" />
       <label for="over_label">Over Label</label>
     </FloatLabel>
-  </main>
+  </div>
 </template>
