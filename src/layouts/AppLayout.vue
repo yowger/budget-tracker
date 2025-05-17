71bc707e391b1a9a4602255fad955e9097f23378
Ver0018 <rogerpantilyowger@gmail.com>
2025-05-17 23:35:31 +0800

    add layout components

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <div>topbar</div>
-    <div>sidebar</div>
-    <div>
-      <router-view></router-view>
-      <div>footer</div>
+    <app-topbar></app-topbar>
+    <app-sidebar></app-sidebar>
+    <div class="flex flex-col min-h-screen justify-between pt-24 px-8 pb-0">
+      <div class="flex-1 pb-8 bg-blue-200">
+        <router-view></router-view>
+      </div>
+      <app-footer></app-footer>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppFooter from '@/layouts/AppFooter.vue'
+import AppSidebar from '@/layouts/AppSidebar.vue'
+import AppTopbar from '@/layouts/AppTopbar.vue'
+</script>
