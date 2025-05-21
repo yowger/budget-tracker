cdb5a503e5774eff6d2905cd3d0c091b0b4eee4b
Ver0018 <rogerpantilyowger@gmail.com>
2025-05-21 20:17:28 +0800

    style update

--- src/layouts/AppTopbar.vue
+++ src/layouts/AppTopbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed top-0 left-0 h-14 w-full px-4 flex items-center justify-between text-color bg-white dark:bg-surface-800"
+    class="fixed top-0 left-0 h-14 w-full px-5 flex items-center justify-between text-color bg-white dark:bg-surface-800"
   >
     <div class="inline-flex gap-1 items-center">
       <Button v-on:click="layout.toggleMenu()" icon="pi pi-bars" variant="text"></Button>
--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,51 @@ import { createApp } from 'vue'
 
 import App from '@/App.vue'
 import router from './router'
+import { definePreset } from '@primeuix/themes'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}',
+    },
+  },
+  components: {
+    card: {
+      colorScheme: {
+        light: {
+          root: {
+            borderRadius: '{borderRadius.sm}',
+            shadow: '{shadow.sm}',
+          },
+        },
+        dark: {
+          root: {
+            background: '{surface.800}',
+          },
+        },
+      },
+    },
+  },
+})
+
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: MyPreset,
     options: {
       darkModeSelector: '.my-app-dark',
       cssLayer: {
