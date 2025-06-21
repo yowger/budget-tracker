6afc3096580c4868b1317dd1ac1642d98e0a654a
Ver0018 <rogerpantilyowger@gmail.com>
2025-06-22 04:34:36 +0800

    added stepper

--- src/layouts/setup/SetupLayout.vue
+++ src/layouts/setup/SetupLayout.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="min-h-screen bg-surface-200 dark:bg-surface-900">
     <setup-topbar>top bar</setup-topbar>
+    <setup-stepper></setup-stepper>
     <router-view></router-view>
   </div>
 </template>
---
+++ src/layouts/setup/SetupStepper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full flex">
+    <template v-for="(label, index) in steps">
+      <div class="flex-1">
+        <div class="flex-1 h-2" :class="index === step ? 'bg-blue-500' : 'bg-gray-300'"></div>
+
+        <div class="mt-2 flex items-center">
+          <span class="text-xs text-gray-600 font-medium">
+            {{ label }}
+          </span>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+const step = 1
+const steps = ['Group', 'Currency', 'Icons', 'Complete']
+</script>
